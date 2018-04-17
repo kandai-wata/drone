e92c0f2e8ceba47896c65b995f6802f1eb82f26b
Kandai Watanabe <kandai.wata@gmail.com>
2018-04-18 00:54:07 +0900

    stopped outputting x,y,d in mutate function

--- mutate.m
+++ mutate.m
@@ -1,9 +1,11 @@
 function d=mutate(offs, mutprop, bound, rng)%uniform mutation.
-[pops,numvar]=size(offs);
-mut=round(mutprop*pops*numvar)  % 何回for文回すか
-for i=1:mut
-    x=ceil(rand*pops)
-    y=ceil(rand*numvar)
-    offs(x,y)=bound(y,1)+rand*rng(y);
+  [pops,numvar]=size(offs);
+  mut=round(mutprop*pops*numvar)  % 何回for文回すか
+  for i=1:mut
+      x=ceil(rand*pops);
+      y=ceil(rand*numvar);
+      offs(x,y)=bound(y,1)+rand*rng(y);
+  end
+  d=offs;
+
 end
-d=offs
