d6d9bb5efc7c14686b11b86b4f1420ffddbef355
Kandai Watanabe <kandai.wata@gmail.com>
2018-04-18 10:46:21 +0900

    added semicolumn

--- mutate.m
+++ mutate.m
@@ -1,6 +1,5 @@
 function d=mutate(offs, mutprop, bound, rng)%uniform mutation.
   [pops,numvar]=size(offs);
-  mut=round(mutprop*pops*numvar)  % 何回for文回すか
   for i=1:mut
       x=ceil(rand*pops);
       y=ceil(rand*numvar);
